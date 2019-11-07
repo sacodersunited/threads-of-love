5a80266b123b300faeab3a6a65e3399dbddade01
Frank Pigeon <frank.pigeonjr@gmail.com>
2019-11-06 19:57:09 -0600

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Threads of Love San Antonio`,
+    description: `Threads of Love San Antonio`,
+    author: `@sacodersunited`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
