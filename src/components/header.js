35cd512bb386b9b4d5298e1a7fc28745f07c47a0
Frank Pigeon <frank.pigeonjr@gmail.com>
2019-11-11 19:14:06 -0600

    adds Bootstrap and navigation

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,38 @@
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { Link } from "gatsby"
+import { Navbar, Nav, Button } from "react-bootstrap"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
+  <Navbar bg="light" expand="lg">
+    <Link to="/">
+      <Navbar.Brand> {siteTitle}</Navbar.Brand>
+    </Link>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Link to="/our-work" className="nav-link" activeClassName="active">
+          Our Work
         </Link>
-      </h1>
-    </div>
-  </header>
+        <Link to="/about" className="nav-link" activeClassName="active">
+          About Us
+        </Link>
+        <Link to="/get-involved" className="nav-link" activeClassName="active">
+          Get Involved
+        </Link>
+        <Link to="/volunteers" className="nav-link" activeClassName="active">
+          Volunteers
+        </Link>
+        <Link to="/calendar" className="nav-link" activeClassName="active">
+          Calendar
+        </Link>
+      </Nav>
+      <Link to="/donate">
+        <Button variant="primary">Donate Now</Button>
+      </Link>
+    </Navbar.Collapse>
+  </Navbar>
 )
 
 Header.propTypes = {
@@ -36,7 +40,7 @@ Header.propTypes = {
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+  siteTitle: `TOL`,
 }
 
 export default Header
