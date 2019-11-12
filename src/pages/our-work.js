94a181aa2ceca6a3b843ea3572ea121719adcc16
Paul Castillo <azrael7@gmail.com>
2019-11-11 23:45:10 -0600

    Adds our work layout to project.

--- src/pages/our-work.js
+++ src/pages/our-work.js
@@ -2,17 +2,12 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import OurWorkLayout from "../components/ourWorkLayout"
 
 const OurWork = () => (
   <Layout>
     <SEO title="Our Work" />
-    <h1>Hi from the Our Work Page</h1>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate
-      quaerat qui minima dolorem architecto non, delectus aperiam distinctio
-      esse veritatis porro vero possimus illo officia mollitia maxime ratione
-      totam debitis.
-    </p>
+    <OurWorkLayout />
   </Layout>
 )
 
