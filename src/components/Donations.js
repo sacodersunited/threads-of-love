c8a05cf06ba3d4d34693be42edc2e259b7d9d64f
Paul Castillo <azrael7@gmail.com>
2019-11-30 23:03:19 -0600

    Added hospitals.

--- src/components/Donations.js
+++ src/components/Donations.js
@@ -2,7 +2,7 @@ import React, { Component } from "react"
 import { Jumbotron, Container, Col, Row } from "react-bootstrap"
 import CountUp from "react-countup"
 import VisibilitySensor from "react-visibility-sensor"
-import { FaRegLifeRing } from "react-icons/fa"
+import { FaRegLifeRing, FaHospital } from "react-icons/fa"
 
 class Donations extends Component {
   state = {
@@ -20,7 +20,49 @@ class Donations extends Component {
       <Jumbotron style={{ background: "#007bff" }}>
         <Container>
           <Row>
-            <Col md={12} style={{ margin: "0 auto" }}>
+            <Col md={6} style={{ margin: "0 auto" }}>
+              <div style={{ textAlign: "center" }}>
+                <div style={{ color: "white", display: "inline" }}>
+                  <FaHospital size={100} style={{ padding: "5px" }} />
+                </div>
+                <VisibilitySensor
+                  onChange={this.onVisibilityChange}
+                  offset={{
+                    top: 10,
+                  }}
+                  delayedCall
+                >
+                  <CountUp
+                    delay={2}
+                    end={this.state.didViewCountUp ? 7 : 0}
+                    size={300}
+                    separator=","
+                    duration="3"
+                    style={{
+                      fontSize: "-webkit-xxx-large",
+                      paddingLeft: "10px",
+                      paddingRight: "10px",
+                      color: "white",
+                      verticalAlign: "middle",
+                      fontFamily: "cursive",
+                    }}
+                  />
+                </VisibilitySensor>
+                <span
+                  style={{
+                    fontSize: "xx-large",
+                    color: "white",
+                    fontWeight: "900px",
+                    display: "inline",
+                    marginRight: "50px",
+                    verticalAlign: "middle",
+                  }}
+                >
+                  South Texas hospitals
+                </span>
+              </div>
+            </Col>
+            <Col md={6}>
               <div style={{ textAlign: "center" }}>
                 <div style={{ color: "white", display: "inline" }}>
                   <FaRegLifeRing size={100} style={{ padding: "5px" }} />
