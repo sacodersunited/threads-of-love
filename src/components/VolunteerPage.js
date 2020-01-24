a5490698d3994094350e389ff07cf0e58ee506c5
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-01-24 00:30:30 -0600

    adds Board members and volunteers

---
+++ src/components/VolunteerPage.js
@@ -0,0 +1,47 @@
+import React from "react"
+import { Container, Row, Col, Card, Image } from "react-bootstrap"
+import { css } from "@emotion/core"
+
+export const VolunteerPage = ({ boardMembers, images }) => (
+  <Container>
+    <h1>Board of Directors</h1>
+    <Row className="mt-5 mb-5">
+      {boardMembers.map(user => (
+        <Col className="mb-3" key={user.id}>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={user.imgSrc} />
+            <Card.Body
+              css={css`
+                min-height: 130px;
+              `}
+            >
+              <Card.Title>{user.name}</Card.Title>
+              <Card.Text>
+                <h4 className="text-uppercase h6 text-muted">
+                  {user.position}
+                </h4>
+                {user.text}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+    <h2>Volunteers</h2>
+    <Row className="mt-5 mb-5">
+      {images.map(image => (
+        <Col xs={6} md={3} key={image.id}>
+          <Image
+            src={image.node.secure_url.replace(
+              "upload/",
+              "upload/c_scale,w_800,h_640/"
+            )}
+            alt="image.id"
+            thumbnail
+            fluid
+          />
+        </Col>
+      ))}
+    </Row>
+  </Container>
+)
--- src/pages/volunteers.js
+++ src/pages/volunteers.js
@@ -1,78 +1,12 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { Container, Row, Col, Card } from "react-bootstrap"
+import VolunteerStaticQuery from "../components/volunteerStaticQuery"
 
-const data = [
-  {
-    name: "Jane Doe",
-    position: "Founder & CEO",
-    imgSrc: "https://randomuser.me/api/portraits/women/23.jpg",
-    text:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
-  },
-  {
-    name: "John Doe",
-    position: "Co-founder & CTO",
-    imgSrc: "https://randomuser.me/api/portraits/men/24.jpg",
-    text:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
-  },
-  {
-    name: "Jenny Popovich",
-    position: "Volunteer Coordinator",
-    imgSrc: "https://randomuser.me/api/portraits/women/56.jpg",
-    text:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
-  },
-  {
-    name: "Jimmy Palon",
-    position: "Outreach",
-    imgSrc: "https://randomuser.me/api/portraits/men/88.jpg",
-    text:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
-  },
-
-  {
-    name: "Janet Donohon",
-    position: "Public Relations",
-    imgSrc: "https://randomuser.me/api/portraits/women/64.jpg",
-    text:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
-  },
-  {
-    name: "Jeremy Riddle",
-    position: "IT Director",
-    imgSrc: "https://randomuser.me/api/portraits/men/34.jpg",
-    text:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
-  },
-]
-
-const Volunteers = () => (
+const Volunteers = ({ images }) => (
   <Layout>
     <SEO title="Volunteers" />
-    <Container>
-      <h1>Board of Directors</h1>
-      <Row className="mt-5 mb-5">
-        {data.map(user => (
-          <Col className="mb-3">
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={user.imgSrc} />
-              <Card.Body>
-                <Card.Title>{user.name}</Card.Title>
-                <Card.Text>
-                  <h4 className="text-uppercase h6 text-muted">
-                    {user.position}
-                  </h4>
-                  {user.text}
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </Container>
+    <VolunteerStaticQuery />
   </Layout>
 )
 
