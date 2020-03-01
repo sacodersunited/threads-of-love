339835cede2af1fe126051319cf1c3484cbd4d95
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-02-29 21:42:06 -0600

    footer styles for mobile
    vol page fixes for mobile

--- src/components/footer.js
+++ src/components/footer.js
@@ -15,45 +15,47 @@ const Footer = () => (
       `}
     >
       <Container>
-        <Col md={4}>
-          <Link to="/">
-            <Navbar.Brand>Threads of Love</Navbar.Brand>
-          </Link>
-          <p className="text-white">
-            {" "}
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org" className="text-white">
-              Gatsby
+        <Row>
+          <Col md={4}>
+            <Link to="/">
+              <Navbar.Brand>Threads of Love</Navbar.Brand>
+            </Link>
+            <p className="text-white">
+              {" "}
+              © {new Date().getFullYear()}, Built with
+              {` `}
+              <a href="https://www.gatsbyjs.org" className="text-white">
+                Gatsby
+              </a>
+            </p>
+          </Col>
+          <Col md={2} className="text-white">
+            <Link className="text-white nav-link" to="/about">
+              About Us
+            </Link>
+            <Link className="text-white nav-link" to="/get-involved">
+              Volunteer
+            </Link>
+            <Link className="text-white nav-link" to="/donate">
+              Donate
+            </Link>
+          </Col>
+          <Col md={2} className="text-white">
+            <a className="text-white nav-link" href="https://facebook.com">
+              <FaFacebookF /> Facebook
             </a>
-          </p>
-        </Col>
-        <Col md={2} className="text-white">
-          <Link className="text-white nav-link" to="/about">
-            About Us
-          </Link>
-          <Link className="text-white nav-link" to="/get-involved">
-            Volunteer
-          </Link>
-          <Link className="text-white nav-link" to="/donate">
-            Donate
-          </Link>
-        </Col>
-        <Col md={2} className="text-white">
-          <a className="text-white nav-link" href="https://facebook.com">
-            <FaFacebookF /> Facebook
-          </a>
-        </Col>
-        <Col md={4} className="text-white">
-          <address>
-            2477 N Loop 1604 East, San Antonio, TX 78232 <br />
-            c/o Linda Coward
-          </address>
+          </Col>
+          <Col md={4} className="text-white">
+            <address>
+              2477 N Loop 1604 East, San Antonio, TX 78232 <br />
+              c/o Linda Coward
+            </address>
 
-          <a href="mailto:threadsoflove@gmail.com" className="text-white">
-            tolatcbc@gmail.com
-          </a>
-        </Col>
+            <a href="mailto:threadsoflove@gmail.com" className="text-white">
+              tolatcbc@gmail.com
+            </a>
+          </Col>
+        </Row>
       </Container>
     </Navbar>
     <nav
