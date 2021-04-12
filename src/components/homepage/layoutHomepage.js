b2be13754024f5f8e25a8204b889f58c86919b56
Paul <azrael7@gmail.com>
2021-04-12 11:08:25 -0500

    Removed ourWork pages no longer used.

--- src/components/homepage/layoutHomepage.js
+++ src/components/homepage/layoutHomepage.js
@@ -10,7 +10,6 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import { Header } from "../header"
 import Footer from "../footer"
-import { css } from "@emotion/core"
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
 const LayoutHomepage = ({ children }) => {
